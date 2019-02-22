0a13f570b6f3473d4c4b3623b8708ac910092e53
Brian Wong <brianwong@zipcoders-MacBook-Pro.local>
2019-02-22 08:37:57 -0500

    Finished tests

--- src/main/java/io/zipcoder/Compare.java
+++ src/main/java/io/zipcoder/Compare.java
@@ -5,7 +5,7 @@ import java.util.Comparator;
 public class Compare implements Comparator<Pet> {
 
     public int compare(Pet o1, Pet o2) {
-        int result;
+        int result = 0;
        /* if(o1.compareTo(o2) ==0) {
            return o1.getName().compareTo(o2.getName());
         } else {
@@ -13,15 +13,25 @@ public class Compare implements Comparator<Pet> {
         } */
 
         if (o1 instanceof Cat) {
-            return -1;
-        } else if (o2 instanceof Turtle) {
-            return 1;
+            if (!(o2 instanceof Cat))
+                result = -1;
+        } else if (o1 instanceof Turtle) {
+            if (!(o1 instanceof Turtle)) {
+                result = 1;
+            }
+        } else if (o1 instanceof Dog) {
+            if (o2 instanceof Cat) {
+                result = 1;
+            } else if (o2 instanceof Turtle) {
+                result = -1;
+            }
         } else {
             if (o1.compareTo(o2) == 0) {
-                return o1.getName().compareTo(o2.getName());
+                result = o1.getName().compareTo(o2.getName());
             } else {
-                return o1.compareTo(o2);
+                result = o1.compareTo(o2);
             }
         }
+        return result;
     }
 }
